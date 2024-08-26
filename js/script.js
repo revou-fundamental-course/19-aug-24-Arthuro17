184310b07e1ec19a88cd783667fa58bb8773a6b9
Arthuro Owen <arthuroowen17@gmail.com>
2024-08-27 02:01:54 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // Ini JS
+
+function replaceName(){
+    let name = prompt("Siapakah nama anda?", "");
+    document.getElementById("name").innerHTML = name
+}
+
+document.getElementById("tombol").addEventListener("click", function() {
+    replaceName();
+})
